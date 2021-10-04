f58ee1caffd516826bcae32d04ce84e8cd3b193c
Andrew Ellison <acannonellison@gmail.com>
2021-10-04 09:43:28 -0700

    Added quiz button functionality, quiz selectors, timer, and start button.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,20 @@
 var ansList = document.querySelector("#answer-list");
 var qText = document.querySelector("#question");
 var qNumber = document.querySelector("#q-number");
+var quizSel = document.querySelector("#quiz-select");
+var startButton = document.querySelector("#start");
+var coverPage = document.querySelector("#cover-page");
 
+var quizType = "HTML";
+var shuffledQuiz; // Currently ordered quiz
+var qi = -1; // quiz question index
+var penalty = 3; // Number of seconds to penalize wrong guess
+var timer; // Initialize timer as global to streamline access
+var timerEl = document.querySelector("#timer");
+var quizTime = 20;
+var curTime = quizTime+1;
 
-// Store full quizzes in object of objects in a separate JS file
+var gameState = false;
 
 var results = {
   correct: 0,
@@ -12,23 +23,94 @@ var results = {
   highScore: 0
 };
 
+// Store full quizzes in object of objects in a separate JS file
+
 // question object: {question:"",choices:"",answer:""}, with choice 1 being correct
 // quiz object array of questions
 
-// Change to selector based on nav clicks
-var quiz = htmlQuiz;
+// Default to HTML quiz
+var quiz = quizzes[quizType];
+//results.highScore = quiz.highScore;
 
 function init() {
 
-  populateQuestion(quiz[Math.floor(Math.random()*quiz.length)],1)
+  for (let ii = 0; ii < quizSel.childElementCount; ii++) {
+    quizSel.children[ii].addEventListener("click",selectQuiz);
+  }
 
-  setScoreboard()
+  for (let ii = 0; ii < ansList.childElementCount; ii++) {
+    let button = ansList.children[ii].children[0] 
+    button.addEventListener("click",answerClicked)
+  }
+
+  initQuiz();
+
+}
+
+function selectQuiz(event) {
+
+  quizType = event.target.textContent;
+  quiz = quizzes[quizType]
+
+  if (gameState) {
+    curTime = 0;
+    endQuiz();
+  }
+
+  initQuiz();
+
+  startButton.textContent = "Start " + quizType + " Quiz"
+
+}
+
+function initQuiz() {
+  // Shuffle quiz question list: courtesy of user superluminary on stackoverflow
+  shuffledQuiz = quiz
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+
+  results.correct = 0;
+  results.wrong = 0;
+  results.skips = 0;
+  setScoreboard();
+
+  // Display cover-page over play area until start
+  coverPage.setAttribute("Style","visibility:visible;")
+
+}
+
+function startQuiz() {
+
+  qi = -1;
+  nextQuestion();
+
+  setTimer()
+
+  gameState = true;
+  // Display cover-page over play area until start
+  coverPage.setAttribute("Style","visibility:hidden;")
+}
+
+
+function setTimer() {
+  
+
+  timer = setInterval(function(){
+    curTime = Math.max(curTime-1,0);
+    timerEl.textContent = "Time: " + curTime;
+
+    if (curTime <= 0) {
+      clearInterval(timer);
+      endQuiz();
+    }
+  },1000);
 
 }
 
 function populateQuestion(qObj,number) {
   // qObj = question object: {question:"",answer:"",choices:""}
-  qNumber.textContent = "Question " + number + ":";
+  qNumber.textContent = "Question " + (number+1) + " of " + quiz.length + ":";
   qText.textContent = qObj.question;
 
   // Shuffle choices: courtesy of user superluminary on stackoverflow
@@ -38,9 +120,78 @@ function populateQuestion(qObj,number) {
     .map(({ value }) => value);
 
   for (let ii = 0; ii < ansList.childElementCount; ii++) {
-    ansList.children[ii].children[0].textContent = shuffled[ii];
+    // Current button
+    let button = ansList.children[ii].children[0] 
+    button.textContent = shuffled[ii];
+
+    // answer is always stored as choice 1, set user attribute to flag which button is the correct answer
+    if (shuffled[ii] == qObj.choices[0]) {
+      button.setAttribute('data-correct',"true")
+    } else {
+      button.setAttribute('data-correct',"false")
+    }
   }
 
+}
+
+function answerClicked(event) {
+  let button = event.target;
+
+  if (!button.disabled) {
+    button.disabled = true
+
+    if (button.getAttribute('data-correct')=="true") {
+      button.classList = button.classList + " right-answer"
+      
+      results.correct++
+
+      buttonCycle(true)
+
+      // Call next question on a timeout 1-2s
+      setTimeout(nextQuestion,1000)
+
+    } else {
+      button.classList = button.classList + " wrong-answer"
+      results.wrong++
+
+      // Decrement timer by penalty
+      curTime = Math.max(curTime-penalty,0)
+
+    }
+    setScoreboard()
+
+  }
+
+}
+
+function nextQuestion() {
+
+  if (qi < quiz.length-1) {
+    qi++
+    buttonCycle(false)
+    populateQuestion(shuffledQuiz[qi],qi)
+  } else {
+    // Out of questions, end game
+    endQuiz()
+  }
+  
+  
+
+}
+
+function buttonCycle(disableBool) {
+  for (let ii = 0; ii < ansList.childElementCount; ii++) {
+    // Current button
+    let button = ansList.children[ii].children[0];
+
+    button.disabled = disableBool;
+
+    if (!disableBool) {
+      // reset classes between questions
+      button.classList = "answer"
+    }
+    
+  }
 }
 
 function setScoreboard() {
@@ -53,5 +204,26 @@ function setScoreboard() {
 }
 
 
+function endQuiz() {
+
+  // stop timer
+  clearInterval(timer);
+
+
+  // set high score if applicable
+  if (curTime > results.highScore) {
+    results.highScore = curTime
+  }
+
+  setScoreboard();
+
+  gameState = false;
+
+  // Display cover-page over play area until start
+  coverPage.setAttribute("Style","visibility:visible;")
+
+}
+
 
 init();
+startButton.addEventListener("click",startQuiz);
--- assets/scripts/quizzes.js
+++ assets/scripts/quizzes.js
@@ -1,10 +1,35 @@
 
 // Storing whole quizzes as lists of objects variables, selected by a onclick callback from the nav bar
-htmlQuiz = [
+
+// HTML questions
+var htmlQuiz = [
   {
     question: "What does HTML stand for?",
     choices: ["HyperText Markup Language","High Toughness Metal Life","Hi This is My Language","HTML TML ML L"],
-    answer: "HyperText Markup Language",
+  },
+  {
+    question: "Which of the following tags needs no closing tag?",
+    choices: ["br","h1","nav","section"],
+  },
+  {
+    question: "What is the current version of HTML?",
+    choices: ["5","2","4","7"],
+  },
+  {
+    question: "When was HTML created?",
+    choices: ["1993","1987","2000","1862"],
+  },
+  {
+    question: "Which is an important component of making images accessible?",
+    choices: ["alt tags","interesting graphics","diversity","braille support"],
+  },
+];
+
+// CSS questions
+var cssQuiz = [
+  {
+    question: "What does CSS stand for?",
+    choices: ["Cascading Stylesheets","Common Style Selector","Can Style Something","CSS SS S"],
   },
   {
     question: "Which of the following tags needs no closing tag?",
@@ -12,7 +37,7 @@ htmlQuiz = [
     answer: "br",
   },
   {
-    question: "WHat is the current version of HTML?",
+    question: "What is the current version of HTML?",
     choices: ["5","2","4","7"],
     answer: "5",
   },
@@ -27,3 +52,70 @@ htmlQuiz = [
     answer: "alt tags",
   },
 ]
+
+// JS questions
+var jsQuiz = [
+  {
+    question: "What does HTML stand for?",
+    choices: ["HyperText Markup Language","High Toughness Metal Life","Hi This is My Language","HTML TML ML L"],
+    answer: "HyperText Markup Language",
+  },
+  {
+    question: "Which of the following tags needs no closing tag?",
+    choices: ["br","h1","nav","section"],
+    answer: "br",
+  },
+  {
+    question: "What is the current version of HTML?",
+    choices: ["5","2","4","7"],
+    answer: "5",
+  },
+  {
+    question: "When was HTML created?",
+    choices: ["1993","1987","2000","1862"],
+    answer: "1993",
+  },
+  {
+    question: "Which is an important component of making images accessible?",
+    choices: ["alt tags","interesting graphics","diversity","braille support"],
+    answer: "alt tags",
+  },
+]
+
+// meme quiz
+var memeQuiz = [
+  {
+    question: "What does HTML stand for?",
+    choices: ["HyperText Markup Language","High Toughness Metal Life","Hi This is My Language","HTML TML ML L"],
+    answer: "HyperText Markup Language",
+  },
+  {
+    question: "Which of the following tags needs no closing tag?",
+    choices: ["br","h1","nav","section"],
+    answer: "br",
+  },
+  {
+    question: "What is the current version of HTML?",
+    choices: ["5","2","4","7"],
+    answer: "5",
+  },
+  {
+    question: "When was HTML created?",
+    choices: ["1993","1987","2000","1862"],
+    answer: "1993",
+  },
+  {
+    question: "Which is an important component of making images accessible?",
+    choices: ["alt tags","interesting graphics","diversity","braille support"],
+    answer: "alt tags",
+  },
+]
+
+
+
+quizzes = {
+  HTML: htmlQuiz,
+  CSS: cssQuiz,
+  JS: jsQuiz,
+  Memes: memeQuiz
+}
